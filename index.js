2343ae731d9cb5bf21826bb4a6381832721c6035
Sienna <remicknsisi@gmail.com>
2022-10-01 13:54:07 -0400

    commit tree b0daecfc3e90963ae326d71aa37521926cf71495

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+document.querySelector('main').remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Sisi Remick is the champion'
+
+document.body.append(newHeader)
